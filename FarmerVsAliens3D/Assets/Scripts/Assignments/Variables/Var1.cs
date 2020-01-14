e07e9737db1eb877f20fbb9ab4decbc597da6639
Riley <10838888@my.uvu.edu>
2020-01-13 17:13:27 -0700

    Made Variables in FVA

---
+++ FarmerVsAliens3D/Assets/Scripts/Assignments/Variables/Var1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Var1 : MonoBehaviour
+{
+
+    // Variables
+    string notSeven = "7";
+    float seven = 6.5f;
+    int child = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(child + seven + notSeven);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
