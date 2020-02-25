4e27e6071921211c13a0d5672a70bc3eed092768
Riley <10838888@my.uvu.edu>
2020-02-24 17:11:51 -0700

    Added physics based movement to enemies
    Yay physics

--- FarmerVsAliens3D/Assets/Scripts/Assignments/EnemyMove.cs
+++ FarmerVsAliens3D/Assets/Scripts/Assignments/EnemyMove.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public GameObject player;
-    public Transform target;
+    private Rigidbody enemyRb;
+    public bool isCylinder;
+    //public Transform target;
     public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = player.transform;
+        /*target = player.transform;
         transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);*/
+    }
+
+    void FixedUpdate()
+    {
+        if (isCylinder) {
+            if (transform.rotation.z > 60 || transform.rotation.z < -60)
+            {
+
+            }
+            if (transform.rotation.x > 60 || transform.rotation.x < -60)
+            {
+
+            }
+        }
+        // Change Velocity
+            //enemyRb.velocity = player * moveSpeed;
+        // Add Force
+            enemyRb.AddForce((player.transform.position - transform.position).normalized * moveSpeed);
+        // Move Position
+            //enemyRb.MovePosition((player.transform.position - transform.position).normalized * moveSpeed);
     }
 }
